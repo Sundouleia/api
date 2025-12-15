dc79d116a4172397540dc4d918b9f581c81bbeb3
Cordelia Mist <buzzmodetrix@gmail.com>
2025-12-14 20:10:47 -0800

    add filetype

---
+++ Enums/SMAFileType.cs
@@ -0,0 +1,13 @@
+namespace SundouleiaAPI.Enums;
+
+/// <summary>
+///     The <b>S</b>undouleia <b>M</b>odular <b>A</b>ctor fileType
+/// </summary>
+public enum SMAFileType
+{
+    Full,       // SMAD
+    Base,       // SMAB
+    Outfit,     // SMAO
+    Item,       // SMAI
+    ItemPack    // SMAIP
+}
