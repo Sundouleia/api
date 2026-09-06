2e915f4c8d989e33c3919b1411abad92ab03166c
Cordelia Mist <buzzmodetrix@gmail.com>
2026-09-05 22:32:43 -0700

    prep API for changes

--- Alterations/ModDataUpdate.cs
+++ Alterations/ModDataUpdate.cs
@@ -20,6 +20,14 @@ public record ValidModFileDto(string ResolvedPath, string[] GamePaths, bool IsFi
     public string Hash { get; init; } = string.Empty;
     public OwnedObject Source { get; init; } = OwnedObject.Player;
     public string Link { get; init; } = string.Empty;
+
+    //// The compressed download link if one is present on the server for this file. Should not exist if not a .tex file.
+    //public string? CompressedLink { get; } = string.Empty;
+    //// If the compressed link is null, this tells us if it is in a worker task.
+    //public bool IsCompressing { get; } = false;
+
+    //// Tells the client if this is a .tex file that has an active compression or has a download link for the compressed version.
+    //[IgnoreMember] public bool IsCompressableTexFile => IsCompressing || CompressedLink is not null;
 }
 
 [MessagePackObject(keyAsPropertyName: true)]
